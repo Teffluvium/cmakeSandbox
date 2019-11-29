ace79964c88f07091f8366f7e80b03b7b967ece2
Tim Herrin <teffluvium@gmail.com>
2019-11-29 07:56:47 +0000

    Updates `CMakeLists.txt` and `cmakeSandbox.cpp`
    * Adds flags to identify the operating system

--- cmakeSandbox.cpp
+++ cmakeSandbox.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
-
 int main() {
     std::cout << "Hello World\n";
+#ifdef Darwin
+    std::cout << "You're on Apple\n";
+#else
+    #ifdef Linux
+        std::cout << "You're on Linux\n";
+    #else
+        std::cout << "This ain't a Mac or Linux\n";
+    #endif
+#endif
     return 0;
 }
